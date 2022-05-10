eca3d8fc3434344c13b679b58f25d408a2cb348a
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-05-10 16:02:35 -0400

    Added in AOP/Lombok/Actuator dependencies. Looks like it works!

--- src/main/java/dev/springallergies/entities/User.java
+++ src/main/java/dev/springallergies/entities/User.java
@@ -1,7 +1,14 @@
 package dev.springallergies.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="users")
 public class User {
@@ -16,36 +23,6 @@ public class User {
     @Column(name="pass")
     private String password;
 
-    public User(){}
-    public User(int userId, String userName, String password) {
-        this.userId = userId;
-        this.userName = userName;
-        this.password = password;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     @Override
     public String toString() {
