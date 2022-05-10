dd1f7cb7759c9f5e5ea0f67b37b0833c7768966e
Azriel13 <armorcrow@gmail.com>
2022-05-10 16:22:29 -0500

    Established the repos.

---
+++ src/main/java/dev/springallergies/repos/ItemRepo.java
@@ -0,0 +1,19 @@
+package dev.springallergies.repos;
+
+import dev.springallergies.entities.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Component
+@Repository
+public interface ItemRepo extends JpaRepository<Item,Integer> {
+
+    List<Item> findItemByStatus (String Status);
+    List<Item> findItemBySupplier ( String supplier);
+    List<Item> findItemByPid (int pid);
+
+}
--- src/main/java/dev/springallergies/repos/UserRepo.java
+++ src/main/java/dev/springallergies/repos/UserRepo.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Component
 @Repository
 public interface UserRepo extends JpaRepository<User,Integer> {
-
-
+    List<User> findUserByUserName(String username);
 }
