f399279383b664979ab995e4e37ee8c59707eceb
elliej1994 <102549808+elliej1994@users.noreply.github.com>
2022-05-13 14:29:06 -0400

    -create Potluck Controller, added get by creator id, and delete by pid

--- src/main/java/dev/springallergies/controllers/PotlukkController.java
+++ src/main/java/dev/springallergies/controllers/PotlukkController.java
@@ -5,9 +5,11 @@ import dev.springallergies.entities.Potlukk;
 import dev.springallergies.services.ItemService;
 import dev.springallergies.services.PotluckService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -42,8 +44,19 @@ public class PotlukkController {
         return this.potluckService.updatePotluck(potlukk);
     }
 
+    @DeleteMapping("/potlucks/{pid}")
+    public Boolean deletePotluck(@PathVariable int pid){
+        Potlukk requestedPotluck = potluckService.fetchPotluckByPotID(pid);
+        if(requestedPotluck!=null){
+            potluckService.deletePotluck(requestedPotluck);
+            return true;
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
 
 
+
+    }
 
 
 
