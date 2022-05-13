059b9b75c950f730fae6f1c84b1da1153fbe51df
Azriel13 <armorcrow@gmail.com>
2022-05-13 14:28:50 -0500

    Finished ItemController

---
+++ src/main/java/dev/springallergies/controllers/ItemController.java
@@ -0,0 +1,54 @@
+package dev.springallergies.controllers;
+
+import dev.springallergies.entities.Item;
+import dev.springallergies.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@CrossOrigin(origins="*", maxAge = 3600)
+@Component
+@Controller
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping("/items")
+    @ResponseBody
+    public Item createItems(@RequestBody Item item){
+        return this.itemService.updateItem(item);
+    }
+
+
+    @DeleteMapping("/items/{id}")
+    @ResponseBody
+    public boolean deleteItems(@PathVariable int id){
+        return this.itemService.deleteItem(itemService.fetchItemByItemId(id));
+    }
+
+    @GetMapping("/items")
+    @ResponseBody
+    public List<Item> retrievedItems(@RequestParam String status){
+        if(status == null) {
+            return this.itemService.fetchItems();
+        }else if(status.equals("ownerWanted") || status.equals("guestProvided")){
+            return this.itemService.fetchItemsByStatus(status);
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/items/{pid}")
+    @ResponseBody
+    public List<Item> getByPId(@PathVariable int pid){
+        return this.itemService.fetchItemsByPid(pid);
+    }
+
+}
