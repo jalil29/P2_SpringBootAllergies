9b05e03f7aa67cbe227a8f11d256c0162a5b0afc
jalil <jalil.ahmed.harmony@gmail.com>
2022-05-16 17:48:53 -0500

    Update PotluckApplication.java

--- src/main/java/dev/springallergies/PotluckApplication.java
+++ src/main/java/dev/springallergies/PotluckApplication.java
@@ -11,9 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "dev.springallergies.entities")
 @EnableJpaRepositories(basePackages = "dev.springallergies.repos")
 public class PotluckApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PotluckApplication.class, args);
 	}
-
 }
